7e9f0650f6418904074dbe1ddeedd3c2514b893a
Sergey G <mailgrishy@gmail.com>
2023-07-26 19:46:04 +0200

    feat: convert all arguments to the FQDN

--- main.go
+++ main.go
@@ -1,11 +1,58 @@
 package main
 
 import (
+	"context"
+	"log"
 	"os"
+	"os/signal"
+	"time"
 
-	"github.com/grishy/go-avahi-cname/avahi"
+	"github.com/grishy/go-avahi-cname/publisher"
+	"github.com/miekg/dns"
 )
 
+const TTL = uint32(60 * 10) // seconds
+
 func main() {
-	avahi.PublishCNAME(os.Args[1:])
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	log.Println("Creating publisher")
+	publisher, err := publisher.NewPublisher()
+	if err != nil {
+		log.Fatalf("Can't create publisher: %v", err)
+	}
+
+	log.Println("Formating CNAMEs:")
+	cnames := os.Args[1:]
+	for i, cname := range cnames {
+		if !dns.IsFqdn(cname) {
+			cnames[i] = dns.Fqdn(cname + "." + publisher.Fqdn())
+
+			log.Printf("  > '%s' (added current FQDN)", cnames[i])
+			continue
+		}
+
+		log.Printf("  > '%s'", cname)
+	}
+
+	resendDuration := time.Duration(TTL/2) * time.Second
+	log.Printf("Publishing every %v ans CNAME TTL=%ds.", resendDuration, TTL)
+
+	// To start publishing immediately
+	// https://github.com/golang/go/issues/17601
+	publisher.PublishCNAMES(os.Args[1:], TTL)
+
+	for {
+		select {
+		case <-time.Tick(resendDuration):
+			publisher.PublishCNAMES(os.Args[1:], TTL)
+		case <-ctx.Done():
+			log.Println("Closing publisher...")
+			if err := publisher.Close(); err != nil {
+				log.Fatalf("Can't close publisher: %v", err)
+			}
+			os.Exit(0)
+		}
+	}
 }
---
+++ publisher/publisher.go
@@ -0,0 +1,95 @@
+package publisher
+
+import (
+	"fmt"
+
+	"github.com/godbus/dbus/v5"
+	"github.com/holoplot/go-avahi"
+	"github.com/miekg/dns"
+)
+
+const (
+	// https://github.com/lathiat/avahi/blob/v0.8/avahi-common/defs.h#L343
+	AVAHI_DNS_CLASS_IN = uint16(0x01)
+	// https://github.com/lathiat/avahi/blob/v0.8/avahi-common/defs.h#L331
+	AVAHI_DNS_TYPE_CNAME = uint16(0x05)
+)
+
+type Publisher struct {
+	dbusConn    *dbus.Conn
+	avahiServer *avahi.Server
+	fqdn        string
+	rdataField  []byte
+}
+
+func NewPublisher() (*Publisher, error) {
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		return nil, fmt.Errorf("can't connect to system bus: %v", err)
+	}
+
+	server, err := avahi.ServerNew(conn)
+	if err != nil {
+		return nil, fmt.Errorf("can't create Avahi server: %v", err)
+	}
+
+	avahiFqdn, err := server.GetHostNameFqdn()
+	if err != nil {
+		return nil, fmt.Errorf("can't get FQDN from Avahi: %v", err)
+	}
+
+	fqdn := dns.Fqdn(avahiFqdn)
+
+	// RDATA: a variable length string of octets that describes the resource. CNAME in our case
+	// Plus 1 because it will add null byte at the end
+	rdataField := make([]byte, len(fqdn)+1)
+	_, err = dns.PackDomainName(fqdn, rdataField, 0, nil, false)
+	if err != nil {
+		return nil, fmt.Errorf("can't pack FQDN into RDATA: %v", err)
+	}
+
+	return &Publisher{
+		dbusConn:    conn,
+		avahiServer: server,
+		fqdn:        fqdn,
+		rdataField:  rdataField,
+	}, nil
+}
+
+func (p *Publisher) Fqdn() string {
+	return p.fqdn
+}
+
+func (p *Publisher) PublishCNAMES(cnames []string, ttl uint32) error {
+	group, err := p.avahiServer.EntryGroupNew()
+	if err != nil {
+		return fmt.Errorf("can't create entry group: %v", err)
+	}
+
+	for _, cname := range cnames {
+		err := group.AddRecord(
+			avahi.InterfaceUnspec,
+			avahi.ProtoUnspec,
+			uint32(0), // From Avahi Python bindings https://gist.github.com/gdamjan/3168336#file-avahi-alias-py-L42
+			cname,
+			AVAHI_DNS_CLASS_IN,
+			AVAHI_DNS_TYPE_CNAME,
+			ttl,
+			p.rdataField,
+		)
+		if err != nil {
+			return fmt.Errorf("can't add record to entry group: %v", err)
+		}
+	}
+
+	if err := group.Commit(); err != nil {
+		return fmt.Errorf("can't commit entry group: %v", err)
+	}
+
+	return nil
+}
+
+func (p *Publisher) Close() error {
+	p.avahiServer.Close()
+	return p.dbusConn.Close()
+}
