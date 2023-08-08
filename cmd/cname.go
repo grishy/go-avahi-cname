8d2df426c6dc44ee5243bed37637a6a3c95c1a4d
Sergey G <mailgrishy@gmail.com>
2023-08-08 14:21:11 +0200

    feat: add subdomain

--- cmd/cname.go
+++ cmd/cname.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/grishy/go-avahi-cname/publisher"
+	"github.com/grishy/go-avahi-cname/avahi"
 	"github.com/miekg/dns"
 	"github.com/urfave/cli/v2"
 )
@@ -28,7 +28,7 @@ func formatCname(hostnameFqdn string, cnames []string) []string {
 	return cnames
 }
 
-func publishing(ctx context.Context, publisher *publisher.Publisher, cnames []string, ttl, interval uint32) error {
+func publishing(ctx context.Context, publisher *avahi.Publisher, cnames []string, ttl, interval uint32) error {
 	log.Printf("Publishing every %ds and CNAME TTL %ds", interval, ttl)
 
 	resendDuration := time.Duration(interval) * time.Second
@@ -49,27 +49,14 @@ func publishing(ctx context.Context, publisher *publisher.Publisher, cnames []st
 			}
 		case <-ctx.Done():
 			fmt.Println() // Add new line after ^C
-			log.Println("Closing publisher...")
-			if err := publisher.Close(); err != nil {
-				return fmt.Errorf("can't close publisher: %w", err)
-			}
+			log.Println("Closing publisher")
+			publisher.Close()
 			return nil
 		}
 	}
 }
 
-func runCname(ctx context.Context, cnames []string, fqdn string, ttl, interval uint32) error {
-	log.Println("Creating publisher")
-	publisher, err := publisher.NewPublisher()
-	if err != nil {
-		return fmt.Errorf("can't create publisher: %w", err)
-	}
-
-	if fqdn == "" {
-		log.Println("Getting FQDN from Avahi")
-		fqdn = publisher.Fqdn()
-	}
-
+func runCname(ctx context.Context, publisher *avahi.Publisher, cnames []string, fqdn string, ttl, interval uint32) error {
 	log.Printf("FQDN: %s", fqdn)
 
 	formattedCname := formatCname(fqdn, cnames)
@@ -79,24 +66,24 @@ func runCname(ctx context.Context, cnames []string, fqdn string, ttl, interval u
 func CmdCname(ctx context.Context) *cli.Command {
 	return &cli.Command{
 		Name:  "cname",
-		Usage: "Anounce CNAME records for current host via Avahi. Require DBus connection to Avahi daemon.",
+		Usage: "Anounce CNAME records for host via avahi-daemon",
 		Flags: []cli.Flag{
 			&cli.UintFlag{
 				Name:    "ttl",
 				Value:   600,
 				EnvVars: []string{"CNAME_TTL"},
-				Usage:   "TTL of CNAME record in seconds",
+				Usage:   "TTL of CNAME record in seconds. How long will they be valid.",
 			},
 			&cli.UintFlag{
 				Name:    "interval",
 				Value:   300,
 				EnvVars: []string{"CNAME_INTERVAL"},
-				Usage:   "Interval of publishing CNAME records in seconds",
+				Usage:   "Interval of publishing CNAME records in seconds. How often to send records to other machines.",
 			},
 			&cli.StringFlag{
 				Name:        "fqdn",
 				EnvVars:     []string{"SUBDOMAIN_FQDN"},
-				Usage:       "FQDN which will be used for CNAME. If empty, will be used current FQDN",
+				Usage:       "Where to redirect. If empty, will be used avahi FQDN (current machine).",
 				DefaultText: "hostname.local.",
 			},
 		},
@@ -110,7 +97,18 @@ func CmdCname(ctx context.Context) *cli.Command {
 				return fmt.Errorf("at least one CNAME should be provided")
 			}
 
-			return runCname(ctx, cnames, fqdn, ttl, interval)
+			log.Println("Creating publisher")
+			publisher, err := avahi.NewPublisher()
+			if err != nil {
+				return fmt.Errorf("can't create publisher: %w", err)
+			}
+
+			if fqdn == "" {
+				log.Println("Getting FQDN from Avahi")
+				fqdn = publisher.Fqdn()
+			}
+
+			return runCname(ctx, publisher, cnames, fqdn, ttl, interval)
 		},
 	}
 }
--- cmd/subdomain.go
+++ cmd/subdomain.go
@@ -2,34 +2,16 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"strings"
 
-	"github.com/godbus/dbus/v5"
-	"github.com/holoplot/go-avahi"
+	"github.com/grishy/go-avahi-cname/avahi"
 	"github.com/miekg/dns"
 	"github.com/urfave/cli/v2"
 )
-
-func getFqdn() (string, error) {
-	conn, err := dbus.SystemBus()
-	if err != nil {
-		return "nil", fmt.Errorf("can't connect to system bus: %v", err)
-	}
-
-	server, err := avahi.ServerNew(conn)
-	if err != nil {
-		return "nil", fmt.Errorf("can't create Avahi server: %v", err)
-	}
-
-	avahiFqdn, err := server.GetHostNameFqdn()
-	if err != nil {
-		return "nil", fmt.Errorf("can't get FQDN from Avahi: %v", err)
-	}
-
-	return avahiFqdn, nil
-}
 
 func listen() (*net.UDPConn, error) {
 	addr := &net.UDPAddr{
@@ -45,59 +27,93 @@ func listen() (*net.UDPConn, error) {
 	return conn, nil
 }
 
-func readMessage(ctx context.Context, conn *net.UDPConn) (chan *dns.Msg, chan error) {
+type dnsMsg struct {
+	msg dns.Msg
+	err error
+}
+
+func reader(ctx context.Context, conn *net.UDPConn) chan *dnsMsg {
 	buf := make([]byte, 1500)
 
-	msgCh := make(chan *dns.Msg)
-	errCh := make(chan error)
+	msgCh := make(chan *dnsMsg)
+
+	go func() {
+		for {
+			dnsMsg := &dnsMsg{
+				msg: dns.Msg{},
+			}
+
+			bytesRead, remoteAddress, err := conn.ReadFromUDP(buf)
+			if err != nil {
+				if ctx.Err() != nil {
+					log.Println("Closing reader")
+					close(msgCh)
+					return
+				}
+
+				dnsMsg.err = errors.Join(dnsMsg.err, fmt.Errorf("can't read from UDP from %s: %w", remoteAddress, err))
+				msgCh <- dnsMsg
+				return
+			}
+
+			if err := dnsMsg.msg.Unpack(buf[:bytesRead]); err != nil {
+				dnsMsg.err = errors.Join(dnsMsg.err, fmt.Errorf("can't unpack message: %w", err))
+				msgCh <- dnsMsg
+				continue
+			}
+
+			msgCh <- dnsMsg
+		}
+	}()
+
+	return msgCh
+}
+
+func selectQuestion(fqdn string, qs []dns.Question) (res []string) {
+	for _, q := range qs {
+		if strings.HasSuffix(q.Name, fqdn) {
+			res = append(res, q.Name)
+		}
+	}
+
+	return res
+}
+
+func runSubdomain(ctx context.Context, publisher *avahi.Publisher, fqdn string, ttl uint32) error {
+	log.Printf("FQDN: %s", fqdn)
+
+	log.Println("Create connection to multicast")
+	conn, err := listen()
+	if err != nil {
+		return fmt.Errorf("can't create connection: %w", err)
+	}
+
+	msgCh := reader(ctx, conn)
 
 	go func() {
 		<-ctx.Done()
 		fmt.Println() // Add new line after ^C
-		log.Println("Closing reader")
-
-		conn.Close()
-		close(msgCh)
-		close(errCh)
-	}()
-
-	go func() {
-		for {
-			read, addr, err := conn.ReadFromUDP(buf)
-			if err != nil {
-				errCh <- fmt.Errorf("can't read from UDP from %s: %w", addr, err)
-				continue
-			}
-
-			msg := new(dns.Msg)
-			if err := msg.Unpack(buf[:read]); err != nil {
-				errCh <- fmt.Errorf("can't unpack message from %s: %w", addr, err)
-				continue
-			}
-
-			msgCh <- msg
+		log.Println("Closing connection")
+		if err := conn.Close(); err != nil {
+			log.Printf("Can't close connection: %v", err)
 		}
 	}()
 
-	return msgCh, errCh
-}
+	log.Println("Start listening")
+	for m := range msgCh {
+		msg := m.msg
+		if m.err != nil {
+			log.Printf("Error: %v", m.err)
+			continue
+		}
 
-func runSubdomain(ctx context.Context, fqdn string) error {
-	log.Println("Starting subdomain...")
+		found := selectQuestion(fqdn, msg.Question)
 
-	l, err := listen()
-	if err != nil {
-		return fmt.Errorf("can't listen: %w", err)
-	}
-	defer l.Close()
-
-	msgCh, errCh := readMessage(ctx, l)
-	// TODO: Pack to one struct, msg and error
-	_ = errCh
-
-	for msg := range msgCh {
-		if len(msg.Question) > 0 {
-			log.Printf("Received question: %s", msg.Question[0].Name)
+		if len(found) > 0 {
+			if err := publisher.PublishCNAMES(found, ttl); err != nil {
+				log.Printf("Can't publish CNAMEs: %v", err)
+				continue
+			}
 		}
 	}
 
@@ -109,6 +125,12 @@ func CmdSubdomain(ctx context.Context) *cli.Command {
 		Name:  "subdomain",
 		Usage: "Listen for all queries and publish CNAMEs for subdomains",
 		Flags: []cli.Flag{
+			&cli.UintFlag{
+				Name:    "ttl",
+				Value:   600,
+				EnvVars: []string{"CNAME_TTL"},
+				Usage:   "TTL of CNAME record in seconds",
+			},
 			&cli.StringFlag{
 				Name:        "fqdn",
 				EnvVars:     []string{"SUBDOMAIN_FQDN"},
@@ -117,19 +139,22 @@ func CmdSubdomain(ctx context.Context) *cli.Command {
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
+			ttl := uint32(cCtx.Uint("ttl"))
 			fqdn := cCtx.String("fqdn")
 
+			log.Println("Creating publisher")
+			publisher, err := avahi.NewPublisher()
+			if err != nil {
+				return fmt.Errorf("can't create publisher: %w", err)
+			}
+			defer publisher.Close()
+
 			if fqdn == "" {
-				var err error
-				fqdn, err = getFqdn()
-				if err != nil {
-					return fmt.Errorf("can't get FQDN: %w", err)
-				}
+				log.Println("Getting FQDN from Avahi")
+				fqdn = publisher.Fqdn()
 			}
 
-			log.Printf("FQDN: %s", fqdn)
-
-			return runSubdomain(ctx, fqdn)
+			return runSubdomain(ctx, publisher, fqdn, ttl)
 		},
 	}
 }
