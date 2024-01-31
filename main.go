4fd183bc9916d1d69da010fbc7df909008630445
Sergey G <mailgrishy@gmail.com>
2024-01-31 21:51:25 +0100

    fix(publisher): reuse EntryGroup - reset+(re)commit

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/carlmjohnson/versioninfo"
 	"github.com/urfave/cli/v2"
@@ -15,6 +17,15 @@ import (
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 	defer stop()
+
+	// Add a handler for force exiting if we don't exit gracefully (stuck)
+	go func() {
+		<-ctx.Done()
+		time.Sleep(3 * time.Second)
+
+		log.Print("Force exit")
+		os.Exit(1)
+	}()
 
 	app := &cli.App{
 		Name:    "go-avahi-cname",
